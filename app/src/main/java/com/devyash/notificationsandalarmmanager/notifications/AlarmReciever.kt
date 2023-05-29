435952eea26dae42690c78cc781a4c75dab2f947
Yashveer singh <yashveersingh30401@gmail.com>
2023-05-29 15:09:57 +0530

    docs - pending Intent Created

--- app/src/main/java/com/devyash/notificationsandalarmmanager/notifications/AlarmReciever.kt
+++ app/src/main/java/com/devyash/notificationsandalarmmanager/notifications/AlarmReciever.kt
@@ -3,6 +3,7 @@ package com.devyash.notificationsandalarmmanager.notifications
 import android.Manifest
 import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -11,12 +12,19 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.devyash.notificationsandalarmmanager.DestActivity
 import com.devyash.notificationsandalarmmanager.R
 import com.devyash.notificationsandalarmmanager.others.Constants.CHANNELID
 
 class AlarmReciever : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
+
+        val intent= Intent(context!!,DestActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+        val pendingIntent = PendingIntent.getActivity(context!!,0,intent,PendingIntent.FLAG_IMMUTABLE)
 
         val builder =NotificationCompat.Builder(context!!,CHANNELID)
             .setSmallIcon(R.drawable.ic_launcher_background)
@@ -25,6 +33,7 @@ class AlarmReciever : BroadcastReceiver() {
             .setAutoCancel(true)
             .setDefaults(Notification.DEFAULT_ALL)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
 
         val notificationManagerCompat = if (ActivityCompat.checkSelfPermission(
                 context!!,
