7aa51f7b0211e8da71bf5a888610506f31da22d3
Yashveer singh <yashveersingh30401@gmail.com>
2023-05-30 16:04:53 +0530

    docs - Notification Channel Created

--- app/src/main/java/com/devyash/notificationsandalarmmanager/MainActivity.kt
+++ app/src/main/java/com/devyash/notificationsandalarmmanager/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.devyash.notificationsandalarmmanager
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TimePicker
+import android.widget.Toast
 import com.devyash.notificationsandalarmmanager.databinding.ActivityMainBinding
+import com.devyash.notificationsandalarmmanager.notifications.AlarmReciever
 import com.devyash.notificationsandalarmmanager.others.Constants.CHANNELID
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var calendar:Calendar
+    private lateinit var alarmManager:AlarmManager
+    private lateinit var pendingIntent:PendingIntent
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,22 +37,16 @@ class MainActivity : AppCompatActivity() {
         createNotificationChannel()
 
         binding.btnSetTimer.setOnClickListener {
-                showTimePicker()
+            showTimePicker()
         }
 
         binding.btnSetAlarm.setOnClickListener {
-
+            setAlarm()
         }
 
         binding.btnCancelAlarm.setOnClickListener {
-
+            cancelAlarm()
         }
-
-    }
-
-    private fun showTimePicker() {
-
-
 
     }
 
@@ -48,14 +58,73 @@ class MainActivity : AppCompatActivity() {
             val channel = NotificationChannel(CHANNELID, name, importance)
             channel.description = description
 
-            val notificationManager = getSystemService(NotificationManager::class.java)
+            val notificationManager = this?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
 
         }
+    }
+
+
+    private fun showTimePicker() {
+
+        val picker: MaterialTimePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Select Alarm Time")
+            .build()
+
+        picker.show(supportFragmentManager, "ALARM")
+
+        //Setting Up the time to the TextView
+        picker.addOnPositiveButtonClickListener {
+            val selectedHour = if (picker.hour > 12) picker.hour - 12 else picker.hour
+            val amPm = if (picker.hour >= 12) "PM" else "AM"
+            val formattedTime = String.format("%02d : %02d %s", selectedHour, picker.minute, amPm)
+            binding.tvTime.text = formattedTime
+        }
+
+
+        //Setting Up The Calendar
+        calendar = Calendar.getInstance()
+        picker.addOnPositiveButtonClickListener {
+            calendar.set(Calendar.HOUR_OF_DAY, picker.hour)
+            calendar.set(Calendar.MINUTE, picker.minute)
+            calendar.set(Calendar.SECOND, 0)
+            calendar.set(Calendar.MILLISECOND, 0)
+            // Perform any additional actions with the selected time
+            Log.d("CALENDAR",calendar.get(Calendar.HOUR_OF_DAY).toString())
+        }
+
+
+
+    }
+
+    private fun setAlarm() {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReciever::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        Toast.makeText(this, "Alarm Set Successfully", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun cancelAlarm() {
+        val intent = Intent(this,AlarmReciever::class.java)
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+        if(alarmManager == null){
+            alarmManager  =  getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        }
+
+        alarmManager.cancel(pendingIntent)
+        Toast.makeText(this, "Alarm Cancel Successfully", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
+
 }
--- app/src/main/java/com/devyash/notificationsandalarmmanager/notifications/AlarmReciever.kt
+++ app/src/main/java/com/devyash/notificationsandalarmmanager/notifications/AlarmReciever.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.widget.Toast
+import androidx.annotation.RequiresPermission
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -24,27 +25,38 @@ class AlarmReciever : BroadcastReceiver() {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
 
-        val pendingIntent = PendingIntent.getActivity(context!!,0,intent,PendingIntent.FLAG_IMMUTABLE)
+        val pendingIntent = PendingIntent.getActivity(context!!,0,intent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
 
-        val builder =NotificationCompat.Builder(context!!,CHANNELID)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("Testing Title")
-            .setContentText("Testing Description of Notification App")
-            .setAutoCancel(true)
-            .setDefaults(Notification.DEFAULT_ALL)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
+        val builder = context?.let {
+            NotificationCompat.Builder(it,CHANNELID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Testing Title")
+                .setContentText("Testing Description of Notification App")
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+        }
 
-        val notificationManagerCompat = if (ActivityCompat.checkSelfPermission(
-                context!!,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            Toast.makeText(context!!, "Permissino Not Given", Toast.LENGTH_SHORT).show()
-            return
-        }else{
-            NotificationManagerCompat.from(context!!)
-                .notify(123,builder.build())
+        with(NotificationManagerCompat.from(context)) {
+            // notificationId is a unique int for each notification that you must define
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            if (builder != null) {
+                notify(System.currentTimeMillis().toInt(), builder.build())
+            }
         }
 
 
