20779e9b6225012b81a97027e8e12ee40b29ca8c
Yashveer singh <yashveersingh30401@gmail.com>
2023-05-31 18:54:46 +0530

    start - Advance notifications

---
+++ app/src/main/java/com/devyash/notificationsandalarmmanager/MyApp.kt
@@ -0,0 +1,12 @@
+package com.devyash.notificationsandalarmmanager
+
+import android.app.Application
+
+class MyApp:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
